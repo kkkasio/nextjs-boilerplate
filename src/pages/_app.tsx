a1b49d6e8d059114f000bd988ecb1f549dddf93e
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-08-18 20:14:15 -0300

    Nprogress Test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import { AppProps } from 'next/app'
+import Router from 'next/router'
+
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import { AppProps } from 'next/app'
 import GlobalStyles from 'styles/global'
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+  NProgress.configure({ easing: 'ease' })
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,10 +23,15 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with typescript, React, NextJS and Styled Components"
+          content="A simple project st
+          arter to work with typescript, React, NextJS and Styled Components"
         />
+
+        <link rel="stylesheet" href="/nprogress.css" />
       </Head>
+
       <GlobalStyles />
+
       <Component {...pageProps} />
     </>
   )
