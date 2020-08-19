e135cd861bac3ebbbb45228262b447ab7c837e27
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-08-18 22:37:24 -0300

    Plop  Generate files

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,14 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
+
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'React Avançado',
+    description: 'Typescript, ReactJS, NextJS, Styled Components'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
